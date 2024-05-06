264e373d3ac7c855ac6f13a29b6956e6223b5c74
PritamGuptaJITU <71538293+PritamGuptaJITU@users.noreply.github.com>
2024-05-06 22:52:06 +0600

    Add files via upload

---
+++ Style.js
@@ -0,0 +1,15 @@
+import {StyleSheet} from 'react-native'
+export default ExStyles = StyleSheet.create({
+
+
+
+    textBox:{
+        color:'#ffff',
+        fontSize:30,
+        backgroundColor:'red',
+        marginBottom:10,
+        padding:4,
+    }
+
+
+})
